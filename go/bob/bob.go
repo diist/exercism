765fbca9076a7fee3fbd40e0d722f1f812a4c64b
labbec <clement.labbe@anz.com>
2019-12-23 12:20:09 +1100

    Bob is dead

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,15 +1,31 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
+WIP - wanna look at community solutions
 
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package bob contains the Exercism logic for Bob
 package bob
 
-// Hey should have a comment documenting it.
+import "regexp"
+
+// Hey replies like an annoying teenager
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	var response string
+
+	statement := regexp.MustCompile(`[A-Z][a-z ,-]+\.`)
+	shouting := regexp.MustCompile(`[A-Z]{2,}!?`)
+	asking := regexp.MustCompile(`[a-z0-9 ,-]+\?`)
+	forcing := regexp.MustCompile(`[a-z0-9 ,-]+\!`)
+
+	switch {
+	case statement.MatchString(remark):
+		response = "Whatever."
+	case shouting.MatchString(remark):
+		response = "Whoa, chill out!"
+	case asking.MatchString(remark):
+		response = "Sure."
+	case forcing.MatchString(remark):
+		response = "Whatever."
+	default:
+		response = "Shush."
+	}
+
+	return response
 }
